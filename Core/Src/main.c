1e8292b2c0f21c21b735ce6ef3046a54ac5e9b12
alexxmatei <alexxmatei@gmail.com>
2023-06-09 16:42:12 +0300

    Add button pullup, implement debounce

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -31,7 +31,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-//#define MASTER_NODE
+#define MASTER_NODE
 
 #ifndef MASTER_NODE
 #define SLAVE_NODE
@@ -49,7 +49,9 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+#define BUTTON_DEBOUNCE_DELAY_MS 20
+GPIO_PinState lastButtonState = GPIO_PIN_SET;
+uint8_t flagState = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -67,9 +69,9 @@ static void MX_I2C1_Init(void);
 /* USER CODE END 0 */
 
 /**
- * @brief  The application entry point.
- * @retval int
- */
+  * @brief  The application entry point.
+  * @retval int
+  */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -123,26 +125,51 @@ int main(void)
 
     /* USER CODE BEGIN 3 */
 #ifdef MASTER_NODE
-	  GPIO_PinState buttonState = HAL_GPIO_ReadPin(Button_GPIO_Port, Button_Pin);
+	    uint8_t data[] = "\r\nMaster node active!\r\n";
+	    HAL_UART_Transmit(&huart2, data, sizeof(data) - 1, HAL_MAX_DELAY);
+
+	    GPIO_PinState buttonState = HAL_GPIO_ReadPin(Button_GPIO_Port, Button_Pin);
+
+	    // Button debouncing
+	    if (buttonState != lastButtonState) {
+	        HAL_Delay(BUTTON_DEBOUNCE_DELAY_MS);
+	        buttonState = HAL_GPIO_ReadPin(Button_GPIO_Port, Button_Pin);
+
+	    }
+
+
+
+
 
 	      if (buttonState == GPIO_PIN_RESET) {
 
+	    	  flagState = 1;
 	      }
 
-    uint8_t data[] = "\r\nMaster node active!\r\n";
-    HAL_UART_Transmit(&huart2, data, sizeof(data) - 1, HAL_MAX_DELAY);
-    HAL_I2C_Master_Receive(&hi2c1, slaveAddress, master_ledStatus_rx, sizeof(master_ledStatus_rx), HAL_MAX_DELAY);
-    if (strcmp((char*)master_ledStatus_rx, "0") == 0)  {
-    	HAL_I2C_Master_Transmit(&hi2c1, slaveAddress, "1", sizeof(sendData), HAL_MAX_DELAY);
-    } else if (strcmp((char*)master_ledStatus_rx, "1") == 0) {
+	      if (flagState == 1) {
+	    	  uint8_t data[] = "\r\nNice!\r\n";
+	    	  	    	    HAL_UART_Transmit(&huart2, data, sizeof(data) - 1, HAL_MAX_DELAY);
+	    	  	    	    flagState=0;
+	      }
+//
+//	    	    HAL_I2C_Master_Receive(&hi2c1, slaveAddress, master_ledStatus_rx, sizeof(master_ledStatus_rx), HAL_MAX_DELAY);
+//	    	    if (strcmp((char*)master_ledStatus_rx, "0") == 0)  {
+//	    	    	HAL_I2C_Master_Transmit(&hi2c1, slaveAddress, "1", sizeof(sendData), HAL_MAX_DELAY);
+//	    	    } else if (strcmp((char*)master_ledStatus_rx, "1") == 0) {
+//
+//	    	    	HAL_I2C_Master_Transmit(&hi2c1, slaveAddress, "0", sizeof(sendData), HAL_MAX_DELAY);
+//	    	    } else  {
+//
+//	    	    	HAL_I2C_Master_Transmit(&hi2c1, slaveAddress, "2", sizeof(sendData), HAL_MAX_DELAY);
+//	    	    }
+//
+//	    	    HAL_UART_Transmit(&huart2, master_ledStatus_rx, sizeof(master_ledStatus_rx) - 1, HAL_MAX_DELAY);
+//	    	    flagState = 0;
+//	      }
 
-    	HAL_I2C_Master_Transmit(&hi2c1, slaveAddress, "0", sizeof(sendData), HAL_MAX_DELAY);
-    } else  {
+	      lastButtonState = buttonState;
 
-    	HAL_I2C_Master_Transmit(&hi2c1, slaveAddress, "2", sizeof(sendData), HAL_MAX_DELAY);
-    }
 
-    HAL_UART_Transmit(&huart2, master_ledStatus_rx, sizeof(master_ledStatus_rx) - 1, HAL_MAX_DELAY);
 //    HAL_Delay(100);
 #else
 #ifdef SLAVE_NODE
@@ -164,21 +191,21 @@ int main(void)
 }
 
 /**
- * @brief System Clock Configuration
- * @retval None
- */
+  * @brief System Clock Configuration
+  * @retval None
+  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-   */
+  */
   HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-   * in the RCC_OscInitTypeDef structure.
-   */
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
@@ -190,8 +217,9 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-   */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1;
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -203,10 +231,10 @@ void SystemClock_Config(void)
 }
 
 /**
- * @brief I2C1 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_I2C1_Init(void)
 {
 
@@ -227,6 +255,7 @@ static void MX_I2C1_Init(void)
   /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.Timing = 0x00303D5B;
+  hi2c1.Init.OwnAddress1 = 132;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
   hi2c1.Init.OwnAddress2 = 0;
@@ -239,14 +268,14 @@ static void MX_I2C1_Init(void)
   }
 
   /** Configure Analogue filter
-   */
+  */
   if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure Digital filter
-   */
+  */
   if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
   {
     Error_Handler();
@@ -254,13 +283,14 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
- * @brief USART2 Initialization Function
- * @param None
- * @retval None
- */
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -301,13 +331,14 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
+
 }
 
 /**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -320,12 +351,19 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
 
+  /*Configure GPIO pin : Button_Pin */
+  GPIO_InitStruct.Pin = Button_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(Button_GPIO_Port, &GPIO_InitStruct);
+
   /*Configure GPIO pin : LED_GREEN_Pin */
   GPIO_InitStruct.Pin = LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(LED_GREEN_GPIO_Port, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
@@ -333,9 +371,9 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
- * @brief  This function is executed in case of error occurrence.
- * @retval None
- */
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -347,14 +385,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef USE_FULL_ASSERT
+#ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
